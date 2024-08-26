1b1f242d324a9441df3c465c3822b8377518eb3c
ryan smith <r@32k.io>
2024-08-26 11:34:21 -0700

    reorganize logs table indexes

--- src/sql/indexes.sql
+++ src/sql/indexes.sql
@@ -1,7 +1,5 @@
-create unique index if not exists single_chain_config on config(chain_id);
-
-create index if not exists logs_address_topic_idx on logs(block_num desc, address, (topics[1]));
-create index if not exists logs_topic_idx on logs(block_num desc, (topics[1]));
-
-create extension if not exists btree_gin;
-create index if not exists logs_block_num_address_topics_idx on logs using gin (block_num, address, topics);
+create index if not exists logs_address_topic_idx on logs((topics[1]), address, block_num desc);
+create index if not exists logs_topics_2 on logs((topics[2]));
+create index if not exists logs_topics_3 on logs((topics[3]));
+create index if not exists logs_topics_4 on logs((topics[4]));
+create index if not exists logs_block_num on logs(block_num);
