33ae4c138657c8d396e3f3a59fdf7f1f11b18cfc
ryan smith <r@32k.io>
2025-01-21 16:52:01 -0800

    fix deploy

--- be/deploy-test.sh
+++ be/deploy-test.sh
@@ -1,3 +1,2 @@
-ssh ubuntu@ga1 'cp /home/ubuntu/golden-axe/target/release/be /home/ubuntu/be.bak'
-ssh ubuntu@ga1 'bash -l -c "cd golden-axe && git pull && cargo build -p be --release"'
-ssh ubuntu@ga1 'sudo systemctl restart be'
+ssh ubuntu@ga1 'bash -l -c "cd golden-axe-test && git fetch && get reset --hard origin/test && cargo build -p be --release"'
+ssh ubuntu@ga1 'sudo systemctl restart be-test'
