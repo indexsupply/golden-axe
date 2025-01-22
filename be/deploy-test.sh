af44ef80ca71d2f84d2bf2cdc6e56737950e5b75
ryan smith <r@32k.io>
2025-01-21 16:52:58 -0800

    fix deploy again

--- be/deploy-test.sh
+++ be/deploy-test.sh
@@ -1,2 +1,2 @@
-ssh ubuntu@ga1 'bash -l -c "cd golden-axe-test && git fetch && get reset --hard origin/test && cargo build -p be --release"'
+ssh ubuntu@ga1 'bash -l -c "cd golden-axe-test && git fetch && git reset --hard origin/test && cargo build -p be --release"'
 ssh ubuntu@ga1 'sudo systemctl restart be-test'
