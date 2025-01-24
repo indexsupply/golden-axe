c56d9304fb4df092582186996e6b0c9fdd69d211
ryan smith <r@32k.io>
2025-01-24 00:45:58 -0800

    tune for speed

--- ex/src/lib.rs
+++ ex/src/lib.rs
@@ -3,7 +3,7 @@ use pgrx::prelude::*;
 
 ::pgrx::pg_module_magic!();
 
-#[pg_extern]
+#[pg_extern(immutable, parallel_safe)]
 fn abi2json(data: &[u8], desc: &str) -> pgrx::JsonB {
     let param = abi::parse(desc).expect("decoding abi signature");
     let parsed = abi::to_json(data, &param).expect("decoding abi");
