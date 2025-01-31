647c265c894b579ab89fa8c0d35d16d8c83469eb
ryan smith <r@32k.io>
2025-01-30 18:42:03 -0800

    add immutable psafe

--- pg_golden_axe/src/lib.rs
+++ pg_golden_axe/src/lib.rs
@@ -3,7 +3,7 @@ use pgrx::prelude::*;
 
 ::pgrx::pg_module_magic!();
 
-#[pg_extern]
+#[pg_extern(immutable, parallel_safe)]
 fn abi2json(data: &[u8], desc: &str) -> pgrx::JsonB {
     let param = abi::Parameter::parse(desc).expect("unable to parse abi signature");
     let parsed = abi::to_json(data, &param).expect("unable to decode");
