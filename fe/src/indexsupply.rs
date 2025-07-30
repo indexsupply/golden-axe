65138f46476e016e7d84c6f56735ac61c6bd03a5
ryan smith <r@32k.io>
2025-07-30 15:43:27 -0700

    fix client http arg order

--- fe/src/indexsupply.rs
+++ fe/src/indexsupply.rs
@@ -40,12 +40,10 @@ impl Client {
         let resp = self
             .http_client
             .post("https://api.indexsupply.net/query")
-            .query(&[(
-                "chain",
-                chain.to_string(),
-                "api-key",
-                "8d622273f07ea179e6d50177ef6ca94d",
-            )])
+            .query(&[
+                ("chain", chain.to_string()),
+                ("api-key", "8d622273f07ea179e6d50177ef6ca94d".to_string()),
+            ])
             .json(&vec![Request {
                 event_signatures: signatures,
                 query: query.to_string(),
