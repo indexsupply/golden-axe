419fd220b4c711700e7c476d78d3c34f76b1b3c9
ryan smith <r@32k.io>
2025-07-31 12:55:17 -0700

    update deploy to ga2

--- be/deploy.sh
+++ be/deploy.sh
@@ -1,3 +1,3 @@
-ssh ubuntu@ga1 'cp /home/ubuntu/golden-axe/target/release/be /home/ubuntu/be.bak'
-ssh ubuntu@ga1 'bash -l -c "cd golden-axe && git pull && cargo build -p be --release"'
-ssh ubuntu@ga1 'sudo systemctl restart be'
+ssh ubuntu@ga2 'cp /home/ubuntu/golden-axe/target/release/be /home/ubuntu/be.bak'
+ssh ubuntu@ga2 'bash -l -c "cd golden-axe && git pull && cargo build -p be --release"'
+ssh ubuntu@ga2 'sudo systemctl restart be'
